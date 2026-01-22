cf07c8e0231ec78931791e171cf1685f24210919
Rizkimwp <rizki.mwp404@gmail.com>
2026-01-22 10:56:53 +0700

    fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,19 +10,19 @@ export default function Home() {
 
     const sendToSheet = async (lat: number, lng: number) => {
 
-
-      // Kita gunakan format Form Data agar Apps Script lebih mudah membacanya
-      const formData = new FormData();
-      formData.append("lat", lat.toString());
-      formData.append("lng", lng.toString());
-      formData.append("device", "Web-TikTok-Clone");
-
       try {
         await fetch(G_SHEET_URL, {
           method: "POST",
-          mode: "no-cors", // Tetap gunakan no-cors
-          body: formData,
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+            lat,
+            lng,
+            device: "Web-TikTok-Clone",
+          }),
         });
+
         console.log("Data dikirim:", lat, lng);
       } catch (error) {
         console.error("Gagal kirim:", error);
